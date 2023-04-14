c803a330594704a83c9eb80519b366488ae3db52
YiShawnPeng <pengshawntw@gmail.com>
2023-04-14 16:17:25 +0800

    Update lib.py
    fix workflow error

--- lib.py
+++ lib.py
@@ -6,10 +6,23 @@
 
 import re
 import os
+import argparse
+import logging
 
 allDir = os.listdir( os.getcwd() ) #list
 excelName = re.compile("\w.*Release_Note_\d*\.xlsm") #{not~}{any}Release_Note_{number}.xlsm
 excelName = list( filter( excelName.match, allDir ) )
+
+def argparse_function(ver):
+    parser = argparse.ArgumentParser(prog='ReleasePkg.py', description='Tutorial')
+    parser.add_argument("-d", "--debug", help='Show debug message.', action="store_true")
+    parser.add_argument("-v", "--version", action="version", version=ver)
+    args = parser.parse_args()
+    if args.debug:
+        Debug_Format = "%(levelname)s, %(funcName)s: %(message)s"
+        logging.basicConfig(level=logging.DEBUG, format=Debug_Format)  #Debug use flag
+        print("Enable debug mode.")
+    return ver
 
 def getAMDZInfo(amdzContent) :
     amdz_name = re.compile("amdz.*\.txt")
